29d4fc0f9ebfad6f91a98e293f312db03d3f0606
kimyounhoex1 <dordyd11@gmail.com>
2025-07-12 17:29:22 +0900

    my-0 format

--- kroyounho/pintos.c
+++ kroyounho/pintos.c
@@ -1,17 +1,4 @@
-#include "devices/input.h"
-#include "devices/kbd.h"
-#include "devices/serial.h"
-#include "devices/timer.h"
-#include "devices/vga.h"
 #include "threads/init.h"
-#include "threads/interrupt.h"
-#include "threads/io.h"
-#include "threads/loader.h"
-#include "threads/malloc.h"
-#include "threads/mmu.h"
-#include "threads/palloc.h"
-#include "threads/pte.h"
-#include "threads/thread.h"
 #include <console.h>
 #include <debug.h>
 #include <limits.h>
@@ -21,10 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "devices/kbd.h"
+#include "devices/input.h"
+#include "devices/serial.h"
+#include "devices/timer.h"
+#include "devices/vga.h"
+#include "threads/interrupt.h"
+#include "threads/io.h"
+#include "threads/loader.h"
+#include "threads/malloc.h"
+#include "threads/mmu.h"
+#include "threads/palloc.h"
+#include "threads/pte.h"
+#include "threads/thread.h"
 #ifdef USERPROG
+#include "userprog/process.h"
 #include "userprog/exception.h"
 #include "userprog/gdt.h"
-#include "userprog/process.h"
 #include "userprog/syscall.h"
 #include "userprog/tss.h"
 #endif
@@ -38,49 +38,37 @@
 #include "filesys/fsutil.h"
 #endif
 
-/* Page-map-level-4 with kernel mappings only. */
 uint64_t *base_pml4;
 
 #ifdef FILESYS
-/* -f: Format the file system? */
 static bool format_filesys;
 #endif
 
-/* -q: Power off after kernel tasks complete? */
 bool power_off_when_done;
-
 bool thread_tests;
 
 static void bss_init(void);
 static void paging_init(uint64_t mem_end);
-
 static char **read_command_line(void);
 static char **parse_options(char **argv);
 static void run_actions(char **argv);
 static void usage(void);
-
 static void print_stats(void);
+static void power_off(void);  // 변경된 선언 (static)
 
 int main(void) NO_RETURN;
 
-/* Pintos main program. */
 int main(void) {
   uint64_t mem_end;
   char **argv;
 
-  /* Clear BSS and get machine's RAM size. */
   bss_init();
-
-  /* Break command line into arguments and parse options. */
   argv = read_command_line();
   argv = parse_options(argv);
 
-  /* Initialize ourselves as a thread so we can use locks,
-     then enable console locking. */
   thread_init();
   console_init();
 
-  /* Initialize memory system. */
   mem_end = palloc_init();
   malloc_init();
   paging_init(mem_end);
@@ -90,7 +78,6 @@ int main(void) {
   gdt_init();
 #endif
 
-  /* Initialize interrupt handlers. */
   intr_init();
   timer_init();
   kbd_init();
@@ -99,13 +86,11 @@ int main(void) {
   exception_init();
   syscall_init();
 #endif
-  /* Start thread scheduler and enable interrupts. */
   thread_start();
   serial_init_queue();
   timer_calibrate();
 
 #ifdef FILESYS
-  /* Initialize file system. */
   disk_init();
   filesys_init(format_filesys);
 #endif
@@ -116,58 +101,43 @@ int main(void) {
 
   printf("Boot complete.\n");
 
-  /* Run actions specified on kernel command line. */
   run_actions(argv);
 
-  /* Finish up. */
   if (power_off_when_done)
     power_off();
   thread_exit();
 }
 
-/* Clear BSS */
 static void bss_init(void) {
-  /* The "BSS" is a segment that should be initialized to zeros.
-     It isn't actually stored on disk or zeroed by the kernel
-     loader, so we have to zero it ourselves.
-
-     The start and end of the BSS segment is recorded by the
-     linker as _start_bss and _end_bss.  See kernel.lds. */
   extern char _start_bss, _end_bss;
-  memset(&_start_bss, 0, &_end_bss - &_start_bss);
+  memset(&_start_bss, 0, (uintptr_t)&_end_bss - (uintptr_t)&_start_bss); // 수정됨
 }
 
-/* Populates the page table with the kernel virtual mapping,
- * and then sets up the CPU to use the new page directory.
- * Points base_pml4 to the pml4 it creates. */
 static void paging_init(uint64_t mem_end) {
-  uint64_t *pml4, *pte;
-  int perm;
+  uint64_t *pml4;
   pml4 = base_pml4 = palloc_get_page(PAL_ASSERT | PAL_ZERO);
 
   extern char start, _end_kernel_text;
-  // Maps physical address [0 ~ mem_end] to
-  //   [LOADER_KERN_BASE ~ LOADER_KERN_BASE + mem_end].
+
   for (uint64_t pa = 0; pa < mem_end; pa += PGSIZE) {
     uint64_t va = (uint64_t)ptov(pa);
 
-    perm = PTE_P | PTE_W;
+    int perm = PTE_P | PTE_W;
     if ((uint64_t)&start <= va && va < (uint64_t)&_end_kernel_text)
       perm &= ~PTE_W;
 
-    if ((pte = pml4e_walk(pml4, va, 1)) != NULL)
+    uint64_t *pte = pml4e_walk(pml4, va, 1);
+    if (pte != NULL)
       *pte = pa | perm;
   }
 
-  // reload cr3
   pml4_activate(0);
 }
 
-/* Breaks the kernel command line into words and returns them as
-   an argv-like array. */
 static char **read_command_line(void) {
   static char *argv[LOADER_ARGS_LEN / 2 + 1];
-  char *p, *end;
+  char *p;
+  const char *end; // const 추가됨
   int argc;
   int i;
 
@@ -183,7 +153,6 @@ static char **read_command_line(void) {
   }
   argv[argc] = NULL;
 
-  /* Print kernel command line. */
   printf("Kernel command line:");
   for (i = 0; i < argc; i++)
     if (strchr(argv[i], ' ') == NULL)
@@ -195,8 +164,6 @@ static char **read_command_line(void) {
   return argv;
 }
 
-/* Parses options in ARGV[]
-   and returns the first non-option argument. */
 static char **parse_options(char **argv) {
   for (; *argv != NULL && **argv == '-'; argv++) {
     char *save_ptr;
@@ -228,7 +195,6 @@ static char **parse_options(char **argv) {
   return argv;
 }
 
-/* Runs the task specified in ARGV[1]. */
 static void run_task(char **argv) {
   const char *task = argv[1];
 
@@ -245,22 +211,21 @@ static void run_task(char **argv) {
   printf("Execution of '%s' complete.\n", task);
 }
 
-/* Executes all of the actions specified in ARGV[]
-   up to the null pointer sentinel. */
 static void run_actions(char **argv) {
-  /* An action. */
   struct action {
-    char *name;                    /* Action name. */
-    int argc;                      /* # of args, including action name. */
-    void (*function)(char **argv); /* Function to execute action. */
+    char *name;
+    int argc;
+    void (*function)(char **argv);
   };
 
-  /* Table of supported actions. */
   static const struct action actions[] = {
       {"run", 2, run_task},
 #ifdef FILESYS
-      {"ls", 1, fsutil_ls},   {"cat", 2, fsutil_cat}, {"rm", 2, fsutil_rm},
-      {"put", 2, fsutil_put}, {"get", 2, fsutil_get},
+      {"ls", 1, fsutil_ls},
+      {"cat", 2, fsutil_cat},
+      {"rm", 2, fsutil_rm},
+      {"put", 2, fsutil_put},
+      {"get", 2, fsutil_get},
 #endif
       {NULL, 0, NULL},
   };
@@ -269,26 +234,21 @@ static void run_actions(char **argv) {
     const struct action *a;
     int i;
 
-    /* Find action name. */
     for (a = actions;; a++)
       if (a->name == NULL)
         PANIC("unknown action `%s' (use -h for help)", *argv);
       else if (!strcmp(*argv, a->name))
         break;
 
-    /* Check for required arguments. */
     for (i = 1; i < a->argc; i++)
       if (argv[i] == NULL)
         PANIC("action `%s' requires %d argument(s)", *argv, a->argc - 1);
 
-    /* Invoke action and advance. */
     a->function(argv);
     argv += a->argc;
   }
 }
 
-/* Prints a kernel command line help message and powers off the
-   machine. */
 static void usage(void) {
   printf("\nCommand line syntax: [OPTION...] [ACTION...]\n"
          "Options must precede actions.\n"
@@ -320,9 +280,7 @@ static void usage(void) {
   power_off();
 }
 
-/* Powers down the machine we're running on,
-   as long as we're running on Bochs or QEMU. */
-void power_off(void) {
+static void power_off(void) {
 #ifdef FILESYS
   filesys_done();
 #endif
@@ -330,12 +288,11 @@ void power_off(void) {
   print_stats();
 
   printf("Powering off...\n");
-  outw(0x604, 0x2000); /* Poweroff command for qemu */
+  outw(0x604, 0x2000);
   for (;;)
     ;
 }
 
-/* Print statistics about Pintos execution. */
 static void print_stats(void) {
   timer_print_stats();
   thread_print_stats();
